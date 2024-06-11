2f23232d4218e022859baecfba55738d38f67597
Tin Phan <phanthanhtincr7@gmail.com>
2024-06-11 15:00:50 +0700

    up

--- src/app/components/TinyMCE/index.jsx
+++ src/app/components/TinyMCE/index.jsx
@@ -1,12 +1,60 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import { Editor } from "@tinymce/tinymce-react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
+import {Editor} from "@tinymce/tinymce-react";
 import SkeletonTinyMCE from "../SkeletonTinyMCE";
 import NotifyMessage from "_utils/notify";
-import { useDispatch } from "react-redux";
-import { setModal } from "_redux/slice/modalSlice";
+import {useDispatch} from "react-redux";
+import {setModal} from "_redux/slice/modalSlice";
 
 const TINY_MCE_KEY = "zfoufc6act8sgb8212yd14inbh3xn0qtgo5jjiqgvbesb9yc";
+// TinyMCE so the global var exists
+import "tinymce/tinymce";
+// DOM model
+import "tinymce/models/dom/model";
+// Theme
+import "tinymce/themes/silver";
+// Toolbar icons
+import "tinymce/icons/default";
+// Editor styles
+import "tinymce/skins/ui/oxide/skin";
 
+// importing the plugin js.
+// if you use a plugin that is not listed here the editor will fail to load
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/anchor";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/autoresize";
+import "tinymce/plugins/autosave";
+import "tinymce/plugins/charmap";
+import "tinymce/plugins/code";
+import "tinymce/plugins/codesample";
+import "tinymce/plugins/directionality";
+import "tinymce/plugins/emoticons";
+import "tinymce/plugins/fullscreen";
+import "tinymce/plugins/help";
+import "tinymce/plugins/help/js/i18n/keynav/en";
+import "tinymce/plugins/image";
+import "tinymce/plugins/importcss";
+import "tinymce/plugins/insertdatetime";
+import "tinymce/plugins/link";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/media";
+import "tinymce/plugins/nonbreaking";
+import "tinymce/plugins/pagebreak";
+import "tinymce/plugins/preview";
+import "tinymce/plugins/quickbars";
+import "tinymce/plugins/save";
+import "tinymce/plugins/searchreplace";
+import "tinymce/plugins/table";
+import "tinymce/plugins/visualblocks";
+import "tinymce/plugins/visualchars";
+import "tinymce/plugins/wordcount";
+
+// importing plugin resources
+import "tinymce/plugins/emoticons/js/emojis";
+
+// Content styles, including inline UI like fake cursors
+import "tinymce/skins/content/default/content";
+import "tinymce/skins/ui/oxide/content";
 function TinyMCE({
 	value,
 	placeholder,
@@ -46,8 +94,8 @@ function TinyMCE({
 	];
 
 	const rel_list = [
-		{ title: "None", value: "" },
-		{ title: "No Follow", value: "nofollow" },
+		{title: "None", value: ""},
+		{title: "No Follow", value: "nofollow"},
 		{
 			title: "No Follow No Opener No Referrer",
 			value: "nofollow noopener noreferrer",
@@ -76,35 +124,35 @@ function TinyMCE({
 			) {
 				e.stopImmediatePropagation();
 				e.stopPropagation();
-				dispatch(setModal({ isFocused: true }));
+				dispatch(setModal({isFocused: true}));
 			}
 		});
 
 		return () => {
-			dispatch(setModal({ isFocused: false }));
-		}
+			dispatch(setModal({isFocused: false}));
+		};
 	}, [editorRef.current]);
 
 	useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (
-                editorRef.current &&
-                !editorRef.current.contentDocument.contains(event.target) &&
-                !event.target.closest(
-                    ".tox-tinymce, .tox-tinymce-aux, .moxman-window, .tam-assetmanager-root"
-                )
-            ) {
-                dispatch(setModal({ isFocused: false }));
-            }
-        };
+		const handleClickOutside = (event) => {
+			if (
+				editorRef.current &&
+				!editorRef.current.contentDocument.contains(event.target) &&
+				!event.target.closest(
+					".tox-tinymce, .tox-tinymce-aux, .moxman-window, .tam-assetmanager-root"
+				)
+			) {
+				dispatch(setModal({isFocused: false}));
+			}
+		};
 
-        document.addEventListener("click", handleClickOutside);
+		document.addEventListener("click", handleClickOutside);
 
-        return () => {
-            document.removeEventListener("click", handleClickOutside);
-        };
-    }, []);
-	
+		return () => {
+			document.removeEventListener("click", handleClickOutside);
+		};
+	}, []);
+
 	const minHeight = useMemo(
 		() => (type === "comment" ? "min-h-36" : "min-h-max"),
 		[type]
@@ -150,23 +198,18 @@ function TinyMCE({
 							var reader = new FileReader();
 							reader.onload = function () {
 								var id = "blobid1" + new Date().getTime();
-								var blobCache =
-									editorRef.current.editorUpload.blobCache;
+								var blobCache = editorRef.current.editorUpload.blobCache;
 								var base64 = reader.result.split(",")[1];
-								var blobInfo = blobCache.create(
-									id,
-									file,
-									base64
-								);
+								var blobInfo = blobCache.create(id, file, base64);
 
 								blobCache.add(blobInfo);
 
 								/* call the callback and populate the Title field with the file name */
-								cb(blobInfo.blobUri(), { title: file.name });
+								cb(blobInfo.blobUri(), {title: file.name});
 							};
 							reader.readAsDataURL(file);
 						};
-						dispatch(setModal({ isFocused: true }));
+						dispatch(setModal({isFocused: true}));
 						input.click();
 					},
 					paste_data_images: true,
@@ -179,13 +222,14 @@ function TinyMCE({
 					toolbar1: type === "comment" ? toolbarComment : toolbar1,
 					// toolbar2: toolbar2,
 					link_rel_list: [
-						{ title: "Nothing", value: "" },
-						{ title: "No Referrer", value: "noreferrer" },
-						{ title: "No Follow", value: "nofollow" },
-						{ title: "Sponsored", value: "sponsored" },
-						{ title: "UGC", value: "ugc" },
+						{title: "Nothing", value: ""},
+						{title: "No Referrer", value: "noreferrer"},
+						{title: "No Follow", value: "nofollow"},
+						{title: "Sponsored", value: "sponsored"},
+						{title: "UGC", value: "ugc"},
 					],
-					content_style: 'body { font-family: Montserrat, sans-serif; font-size:14px, padding: 12px; }'
+					content_style:
+						"body { font-family: Montserrat, sans-serif; font-size:14px, padding: 12px; }",
 				}}
 				{...rest}
 			/>
